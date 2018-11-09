43967421bcc955f4047925a58de8ac509e3e2d6c
vyeung <vl.yeung@yahoo.com>
2018-11-08 21:53:44 -0500

    added home route and cmpt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from "react-router-dom";
 import Layout from "./hoc/layout";
+import Home from "./components/homepage/homepage";
 
 const App = () => {
   return (
     <Layout>
-      
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
     </Layout>
   );
 }
---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./homepage.css";
+
+const homepage = () => {
+  return (
+    <div className="home_wrapper">
+
+    </div>
+  );
+};
+
+export default homepage;
