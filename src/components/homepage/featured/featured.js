319edc57bfa1ae26189c051f12c0b238840ac9df
vyeung <vl.yeung@yahoo.com>
2018-11-08 21:55:17 -0500

    added featured cmpt to home

---
+++ src/components/homepage/featured/featured.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./featured.css";
+
+const featured = () => {
+  return (
+    <div className="featured_wrapper">
+
+    </div>
+  );
+};
+
+export default featured;
--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./homepage.css";
+import Featured from "./featured/featured";
 
 const homepage = () => {
   return (
     <div className="home_wrapper">
-
+      <Featured />
     </div>
   );
 };
