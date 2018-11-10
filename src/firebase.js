0d71ab3224b8c8f4b6624f12ac49ea8e97b69bc3
vyeung <vl.yeung@yahoo.com>
2018-11-10 16:03:05 -0500

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/database";
+
+//see official docs for more info
+var config = {
+  apiKey: "AIzaSyD20wARC_8RRygsk_Y1TBy-uD4hOEog-tY",
+  authDomain: "reactjs-houstonrockets.firebaseapp.com",
+  databaseURL: "https://reactjs-houstonrockets.firebaseio.com",
+  projectId: "reactjs-houstonrockets",
+  storageBucket: "reactjs-houstonrockets.appspot.com",
+  messagingSenderId: "492311369219"
+};
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+const firebaseGames = firebaseDB.ref("games");
+
+//for testing
+// firebaseDB.ref("games").once("value")
+//   .then((snapshot) => {
+//     console.log(snapshot.val());
+//   })
+
+export {
+  firebase,
+  firebaseGames
+}
