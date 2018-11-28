33218b360e984315270bbbc3f98865682a3b5b7b
vyeung <vl.yeung@yahoo.com>
2018-11-27 22:38:11 -0500

    added Login and Dashboard routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 import { Switch, Route } from "react-router-dom";
 import Layout from "./hoc/layout";
 import Home from "./components/homepage/homepage";
+import Login from "./components/login/Login";
+import Dashboard from "./components/admin/dashboard";
 
-const App = () => {
+const App = (props) => {
+  
   return (
     <Layout>
       <Switch>
+        <Route exact component={Dashboard} path="/dashboard" />
+        <Route exact component={Login} path="/login" />
         <Route exact component={Home} path="/" />
       </Switch>
     </Layout>
