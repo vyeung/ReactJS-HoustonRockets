97b6c6f98643b0299e9ba17b3bc74be944534cfd
vyeung <vl.yeung@yahoo.com>
2018-11-29 16:20:03 -0500

    added auth detection in index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter } from "react-router-dom";
+import { firebase } from "./firebase";
 
-const AppWithRoutes = () => {
+const AppWithRoutes = (props) => {
   return (
     <BrowserRouter>
-      <App />
+      <App {...props} />
     </BrowserRouter>
   );
 }
 
-ReactDOM.render(<AppWithRoutes />, document.getElementById('root'));
+firebase.auth().onAuthStateChanged((user) => {
+  console.log(user);
+
+  ReactDOM.render(<AppWithRoutes userInfo={user} />, document.getElementById('root'));
+})
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
