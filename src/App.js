ccd8d29b9675a6859207ac86b1016967fd5c002b
vyeung <vl.yeung@yahoo.com>
2018-12-05 16:13:02 -0500

    added AdminGames route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from "./hoc/layout";
 import Home from "./components/homepage/homepage";
 import Login from "./components/login/Login";
 import Dashboard from "./components/admin/dashboard";
+import AdminGames from "./components/admin/games/AdminGames";
 
 import PrivateRoute from "./components/authRoutes/privateRoutes";
 import PublicRoute from "./components/authRoutes/publicRoutes";
@@ -17,6 +18,7 @@ const App = (props) => {
       <Switch>
         {/*...props will include the userInfo prop from index.js*/}
         <PrivateRoute {...props} exact component={Dashboard} path="/dashboard" />
+        <PrivateRoute {...props} exact component={AdminGames} path="/admin_games" />
 
         <PublicRoute {...props} exact component={Login} path="/login" hasRestrictions={true} />
         <PublicRoute {...props} exact component={Home} path="/" hasRestrictions={false} />
