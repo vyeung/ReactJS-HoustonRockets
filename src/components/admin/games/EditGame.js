0bee87e456934a1b89aa44cf20fd1e0ca967774e
vyeung <vl.yeung@yahoo.com>
2018-12-21 18:08:16 -0500

    added EditGame cmpt

---
+++ src/components/admin/games/EditGame.js
@@ -0,0 +1,287 @@
+import React, { Component } from 'react';
+import "./EditGame.css";
+
+import AdminLayout from "../../../hoc/adminLayout";
+import FormField from "../../utils/formField";
+import validate from "../../utils/validate";
+
+import { firebaseDB, firebaseTeams} from "../../../firebase";
+import firebaseLooper from "../../utils/firebaseLooper";
+
+class EditGame extends Component {
+
+  state = {
+    gameId: "",
+    formType: "",
+    formError: "false",
+    formSubmitMsg: "",
+    teams: [],
+    editGameForm: {
+      date: {
+        htmlTag: "input",
+        value: "",
+        config: {
+          name: "date_input",
+          placeholder: "MM-DD-YYYY"
+        },
+        validation: {
+          required: true,
+          date: true
+        },
+        valid: false,
+        validationMsg: ""
+      },
+      home: {
+        htmlTag: "select",
+        value: "",
+        config: {
+          name: "select_home_team",
+          type: "select",
+          options: []
+        },
+        validation: {
+          required: true 
+        },
+        valid: false,
+        validationMsg: ""
+      },
+      homeScore: {
+        htmlTag: "input",
+        value: "",
+        config: {
+          name: "homeScore_input",
+          placeholder: "Number..."
+        },
+        validation: {
+          required: true,
+          number: true
+        },
+        valid: false,
+        validationMsg: ""
+      },
+      away: {
+        htmlTag: "select",
+        value: "",
+        config: {
+          name: "select_away_team",
+          type: "select",
+          options: []
+        },
+        validation: {
+          required: true 
+        },
+        valid: false,
+        validationMsg: ""
+      },
+      awayScore: {
+        htmlTag: "input",
+        value: "",
+        config: {
+          name: "awayScore_input",
+          placeholder: "Number..."
+        },
+        validation: {
+          required: true,
+          number: true
+        },
+        valid: false,
+        validationMsg: ""
+      },
+      result: {
+        htmlTag: "select",
+        value: "",
+        config: {
+          name: "select_result",
+          type: "select",
+          options: [
+            {key:"W", value:"Win"},
+            {key:"L", value:"Loss"},
+            {key:"T", value:"Tie"},
+            {key:"C", value:"Cancelled"},
+            {key:"n/a", value:"TBD"}
+          ]
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        validationMsg: ""
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.getTeamsFromDB();
+    
+    //match object is provided by react-router
+    const gameId = this.props.match.params.id;
+    
+    //find the target game in the database
+    firebaseDB.ref(`games/${gameId}`).once("value")
+      .then((snapshot) => {
+        const game = snapshot.val();
+        //console.log(game);
+        this.fillFieldsWithGameInfo(game, gameId);
+      })
+  }
+
+  getTeamsFromDB = () => {
+    firebaseTeams.once("value")
+      .then((snapshot) => {
+        const teams = firebaseLooper(snapshot);
+        const formattedTeams = [];
+
+        //push objects with key, value pairs
+        for(var i=0; i<teams.length; i++) {
+          formattedTeams.push({
+            key: teams[i].shortName,
+            value: teams[i].shortName
+          });
+        }
+
+        //sort alphabetically by shortName
+        formattedTeams.sort((a, b) => {
+          return a.value.localeCompare(b.value);
+        })
+
+        const updateTeamSelects = {...this.state.editGameForm};
+        updateTeamSelects.home.config.options = formattedTeams;
+        updateTeamSelects.away.config.options = formattedTeams;
+
+        this.setState({editGameForm: updateTeamSelects});
+      })
+  }
+
+  fillFieldsWithGameInfo = (game, gameId) => {
+    const updatedForm = {...this.state.editGameForm};
+
+    for(var key in updatedForm) {
+      //when the game field is also in our form
+      if(updatedForm[key] && game[key]) {
+        updatedForm[key].value = game[key];
+        updatedForm[key].valid = true;
+      }
+    }
+
+    this.setState({
+      editGameForm: updatedForm,
+      gameId: gameId
+    });
+  }
+
+  submitFormHandler = (event) => {
+    event.preventDefault();
+    
+    let dataToSubmit = {};
+    let overallFormIsValid = true;
+
+    for (var key in this.state.editGameForm) {
+      dataToSubmit[key] = this.state.editGameForm[key].value;
+      
+      //don't need to continue loop if a field is submitted with errors
+      if(this.state.editGameForm[key].valid === false) {
+        overallFormIsValid = false;
+        break;
+      }
+    }
+
+    if(overallFormIsValid === true) {
+      firebaseDB.ref(`games/${this.state.gameId}`).update(dataToSubmit)
+        .then(() => {
+          this.setState({formSubmitMsg: "Changes Saved!"});
+          setTimeout(() => {
+            this.setState({formSubmitMsg: ""});
+            this.props.history.push("/admin_games");
+          }, 2000);
+        })
+    }
+    else {
+      this.setState({formSubmitMsg: "Please submit valid fields"});
+      setTimeout(() => {
+        this.setState({formSubmitMsg: ""});
+      }, 2000);
+    }
+  }
+
+  updateFormHandler = (elem) => {
+    const updatedForm = {...this.state.editGameForm};
+    updatedForm[elem.myFieldType].value = elem.event.target.value;
+
+    let checkValid = validate(updatedForm[elem.myFieldType]);
+
+    updatedForm[elem.myFieldType].valid = checkValid[0];
+    updatedForm[elem.myFieldType].validationMsg = checkValid[1];
+
+    this.setState({editGameForm: updatedForm});
+  }
+
+  render() {
+    return (
+      <AdminLayout>
+        <div className="editGame_wrapper">
+          <h2>Edit Game</h2>
+
+          <form onSubmit={this.submitFormHandler}>
+            <div className="editGame_grouping">
+              <div className="editGame_label">Game Date</div>
+              <FormField
+                fieldType="date"
+                fieldData={this.state.editGameForm.date}
+                changed={this.updateFormHandler}
+              />
+            </div>
+            
+            <div className="editGame_grouping">
+              <div className="editGame_label">Home Team</div>
+              <FormField
+                fieldType="home"
+                fieldData={this.state.editGameForm.home}
+                changed={this.updateFormHandler}
+              />
+              <div className="editGame_label">Home Score</div>
+              <div className="editGame_score">
+                <FormField
+                  fieldType="homeScore"
+                  fieldData={this.state.editGameForm.homeScore}
+                  changed={this.updateFormHandler}
+                />
+              </div>
+
+              <div className="editGame_label">Away Team</div>
+              <FormField
+                fieldType="away"
+                fieldData={this.state.editGameForm.away}
+                changed={this.updateFormHandler}
+              />
+              <div className="editGame_label">Away Score</div>
+              <div className="editGame_score">
+                <FormField
+                  fieldType="awayScore"
+                  fieldData={this.state.editGameForm.awayScore}
+                  changed={this.updateFormHandler}
+                />
+              </div>
+            </div>
+
+            <div className="editGame_grouping">
+              <div className="editGame_label">Game Result</div>
+              <FormField
+                fieldType="result"
+                fieldData={this.state.editGameForm.result}
+                changed={this.updateFormHandler}
+              />
+            </div>
+
+            <button>Confirm Changes</button>
+
+            <div className="editGame_formSubmit">
+              {this.state.formSubmitMsg}
+            </div>
+          </form>
+        </div>
+      </AdminLayout>
+    );
+  }
+}
+
+export default EditGame;
