3a7465a0245d3969ab966bfc1f21944ecf3a66b1
vyeung <vl.yeung@yahoo.com>
2018-12-21 18:09:08 -0500

    added EditGame route

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { Switch } from "react-router-dom";
 import Layout from "./hoc/layout";
 import Home from "./components/homepage/homepage";
 import Login from "./components/login/Login";
+
 import Dashboard from "./components/admin/dashboard";
 import AdminGames from "./components/admin/games/AdminGames";
+import EditGame from "./components/admin/games/EditGame";
 
 import PrivateRoute from "./components/authRoutes/privateRoutes";
 import PublicRoute from "./components/authRoutes/publicRoutes";
@@ -19,6 +21,7 @@ const App = (props) => {
         {/*...props will include the userInfo prop from index.js*/}
         <PrivateRoute {...props} exact component={Dashboard} path="/dashboard" />
         <PrivateRoute {...props} exact component={AdminGames} path="/admin_games" />
+        <PrivateRoute {...props} exact component={EditGame} path="/admin_games/edit_game/:id" />
 
         <PublicRoute {...props} exact component={Login} path="/login" hasRestrictions={true} />
         <PublicRoute {...props} exact component={Home} path="/" hasRestrictions={false} />
