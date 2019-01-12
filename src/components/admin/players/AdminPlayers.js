de122866d2c8abc4541b6b3d307ec65bb65d1cfe
vyeung <vl.yeung@yahoo.com>
2019-01-11 23:51:56 -0500

    added AdminPlayers cmpt

---
+++ src/components/admin/players/AdminPlayers.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import "./AdminPlayers.css";
+
+import { Link } from "react-router-dom"
+import { firebasePlayers } from "../../../firebase";
+import firebaseLooper from "../../utils/firebaseLooper";
+import AdminLayout from "../../../hoc/adminLayout";
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+class AdminPlayers extends Component {
+
+  state = {
+    isLoading: true,
+    players: []
+  }
+
+  componentDidMount() {
+    firebasePlayers.once("value")
+      .then((snapshot) => {
+        const formattedPlayers = firebaseLooper(snapshot);
+        this.setState({
+          isLoading: false,
+          players: formattedPlayers
+        });
+      })
+  }
+
+  render() {
+    console.log(this.state.players);
+    const titleSize = {fontSize: "30px"};
+    const bodySize = {fontSize: "20px"};
+
+    return (
+      <AdminLayout>
+        <div className="adminPlayers_editMsg">
+          Click on a player name to edit
+        </div>
+
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={titleSize}>No.</TableCell>
+                <TableCell style={titleSize}>NAME</TableCell>
+                <TableCell style={titleSize}>POS</TableCell>
+                <TableCell style={titleSize}>HT</TableCell>
+                <TableCell style={titleSize}>WT</TableCell>
+                <TableCell style={titleSize}>EXP</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {this.state.players ? 
+                this.state.players.map((player, i) => (
+                  <TableRow key={i}>
+                    <TableCell style={bodySize}>
+                      {player.number}
+                    </TableCell>
+                    <TableCell style={bodySize}>
+                      <Link to={`/admin_players/edit_player/${player.id}`}>
+                        {player.firstname + " " + player.lastname}
+                      </Link>
+                    </TableCell>
+                    <TableCell style={bodySize}>
+                      {player.position}
+                    </TableCell>
+                    <TableCell style={bodySize}>
+                      {player.height}
+                    </TableCell>
+                    <TableCell style={bodySize}>
+                      {player.weight} lbs
+                    </TableCell>
+                    <TableCell style={bodySize}>
+                      {player.exp}
+                    </TableCell>
+                  </TableRow>
+                ))
+                :
+                null}
+            </TableBody>
+          </Table>
+        </Paper>
+
+        <div className="adminPlayers_loading">
+          {this.state.isLoading ? 
+            <CircularProgress thickness={7} style={{color:"#d31145"}} /> 
+            : 
+            null}
+        </div>
+      </AdminLayout>
+    );
+  }
+}
+
+export default AdminPlayers;
