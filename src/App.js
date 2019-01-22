4d48ee02c5111c6a45110228523d09d99db5cd94
vyeung <vl.yeung@yahoo.com>
2019-01-22 01:21:27 -0500

    added AddPlayer and EditPlayer routes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import AdminGames from "./components/admin/games/AdminGames";
 import EditGame from "./components/admin/games/EditGame";
 import AddGame from "./components/admin/games/AddGame";
 import AdminPlayers from "./components/admin/players/AdminPlayers";
+import EditPlayer from "./components/admin/players/EditPlayer";
+import AddPlayer from "./components/admin/players/AddPlayer";
 
 import PrivateRoute from "./components/authRoutes/privateRoutes";
 import PublicRoute from "./components/authRoutes/publicRoutes";
@@ -26,6 +28,8 @@ const App = (props) => {
         <PrivateRoute {...props} exact component={EditGame} path="/admin_games/edit_game/:id" />
         <PrivateRoute {...props} exact component={AddGame} path="/admin_games/add_game" />
         <PrivateRoute {...props} exact component={AdminPlayers} path="/admin_players" />
+        <PrivateRoute {...props} exact component={EditPlayer} path="/admin_players/edit_player/:id" />
+        <PrivateRoute {...props} exact component={AddPlayer} path="/admin_players/add_player" />
 
         <PublicRoute {...props} exact component={Login} path="/login" hasRestrictions={true} />
         <PublicRoute {...props} exact component={Home} path="/" hasRestrictions={false} />
