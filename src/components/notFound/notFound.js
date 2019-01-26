faafbc687188ec3f2021ad02c31b86fec63acebb
vyeung <vl.yeung@yahoo.com>
2019-01-26 17:04:53 -0500

    added 404 notFound cmpt

---
+++ src/components/notFound/notFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./notFound.css";
+
+const notFound = () => {
+  return (
+    <div className="notFound_container">
+      <div className="notFound1">
+        404
+      </div>
+      <div className="notFound2">
+        Page not found :(
+      </div>
+    </div>
+  );
+};
+
+export default notFound;
