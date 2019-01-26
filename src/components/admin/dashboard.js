b7916c85021eded584d487a55202aac0143a9819
vyeung <vl.yeung@yahoo.com>
2019-01-25 23:42:59 -0500

    scroll starts at top when going to /dashboard

--- src/components/admin/dashboard.js
+++ src/components/admin/dashboard.js
@@ -4,9 +4,15 @@ import "./dashboard.css";
 import AdminLayout from "../../hoc/adminLayout";
 
 const dashboard = () => {
+
+  const goToTop = () => {
+    window.scrollTo(0, 0);
+  }
+
   return (
     <AdminLayout>
       <div className="dashboard">
+        {goToTop()}
         This is your dashboard
       </div>
     </AdminLayout>
