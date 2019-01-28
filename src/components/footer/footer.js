1ca8788793880942e1938e09ddb1c2e238e916df
vyeung <vl.yeung@yahoo.com>
2019-01-28 17:32:20 -0500

    added autodate function to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,12 +3,17 @@ import "./footer.css";
 import RocketsLogo from "../utils/logo";
 
 const footer = () => {
+
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  }
+
   return (
     <footer className="footer_wrapper">
       <RocketsLogo isLink={false} width="70px" height="70px" />
 
       <div className="footer_description">
-        Houston Rockets. 2018. All Rights Reserved.
+        Houston Rockets. {getCurrentYear()}. All Rights Reserved.
       </div>
     </footer>
   );
