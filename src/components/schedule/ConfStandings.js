522ff1099c1dd975768b86cda4f2d3865f820419
vyeung <vl.yeung@yahoo.com>
2021-06-16 18:10:23 -0400

    add getTeamName util function

--- src/components/schedule/ConfStandings.js
+++ src/components/schedule/ConfStandings.js
@@ -7,15 +7,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import CircularProgress from "@material-ui/core/CircularProgress";
-
-//global hash map
-const teamMap = new Map([
-  ["1610612746", "Clippers"], ["1610612763", "Grizzlies"], ["1610612762", "Jazz"],
-  ["1610612758", "Kings"], ["1610612747", "Lakers"], ["1610612742", "Mavericks"],
-  ["1610612743", "Nuggets"], ["1610612740", "Pelicans"], ["1610612745", "Rockets"],
-  ["1610612759", "Spurs"], ["1610612756", "Suns"], ["1610612760", "Thunder"],
-  ["1610612750", "Timberwolves"], ["1610612757", "Trailblazers"], ["1610612744", "Warriors"]
-]);
+import getTeamName from '../utils/getTeamName';
 
 class ConfStandings extends Component {
   
@@ -26,7 +18,7 @@ class ConfStandings extends Component {
 
   componentDidMount() {
     //using a CORS proxy to get around error
-    fetch("https://cors-anywhere.herokuapp.com/http://data.nba.net/10s/prod/v1/current/standings_conference.json")
+    fetch("https://cors-anywhere.herokuapp.com/http://data.nba.net/prod/v1/current/standings_conference.json")
       .then(res => res.json())
       .catch(error => console.log(error))
         .then(data => {
@@ -76,8 +68,8 @@ class ConfStandings extends Component {
                 {/* black line shows cut off for playoff eligible teams */}
                 {i===7 ? <TableCell style={black}>{i+1}</TableCell> 
                   : <TableCell style={regular}>{i+1}</TableCell>}
-                {i===7 ? <TableCell style={black}>{teamMap.get(team.teamId)}</TableCell> 
-                  : <TableCell style={regular}>{teamMap.get(team.teamId)}</TableCell>}
+                {i===7 ? <TableCell style={black}>{getTeamName(team.teamId)}</TableCell> 
+                  : <TableCell style={regular}>{getTeamName(team.teamId)}</TableCell>}
                 {i===7 ? <TableCell style={black}>{team.win}</TableCell> 
                   : <TableCell style={regular}>{team.win}</TableCell>}
                 {i===7 ? <TableCell style={black}>{team.loss}</TableCell> 
