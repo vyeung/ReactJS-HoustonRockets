9d5b08977c993865e31bb22f08fa9888f9dc1f80
vyeung <vl.yeung@yahoo.com>
2021-07-11 15:24:04 -0400

    add auto scrollToTop on homepage

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -6,9 +6,14 @@ import RecentGames from "./recentGames/recentGames";
 import ViewRoster from "./viewRoster/viewRoster";
 import WinJersey from "./winJersey/winJersey";
 
+const goToTop = () => {
+  window.scrollTo(0, 0);
+}
+
 const homepage = () => {
   return (
     <div className="home_wrapper">
+      {goToTop()}
       <Featured />
       <RecentGames />
       <ViewRoster />
